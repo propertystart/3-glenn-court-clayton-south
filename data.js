0b8a638a649fa2752a9de6d846f9e85ae3de18f2
PropertyStart Admin <info@propertystart.com.au>
2020-03-29 16:43:37 +1100

    Version 1

---
+++ data.js
@@ -0,0 +1,207 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchenmeals",
+      "name": "Kitchen/Meals",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.9117942292920382,
+        "pitch": -0.04674450137406083,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6587446514100801,
+          "pitch": 0.32867755904764806,
+          "rotation": 0,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": 1.718201803539288,
+          "pitch": 0.2570482096316784,
+          "rotation": 0,
+          "target": "2-master-br-with-en-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.597484503045184,
+        "pitch": -0.11387065330655233,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5766792435160895,
+          "pitch": 0.3651857973257364,
+          "rotation": 0,
+          "target": "0-kitchenmeals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master-br-with-en-suite",
+      "name": "Master BR with En-Suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.2551820756137317,
+        "pitch": 0.0963600518713914,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.135912957605882,
+          "pitch": 0.07012126636137239,
+          "rotation": 1.5707963267948966,
+          "target": "0-kitchenmeals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.27177105641933075,
+        "pitch": 0.1121455734927217,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4971975698911297,
+          "pitch": 0.07072435648989561,
+          "rotation": 5.497787143782138,
+          "target": "0-kitchenmeals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9937691047069777,
+        "pitch": 0.30795051257937445,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3442697837393478,
+          "pitch": 0.06039714481520164,
+          "rotation": 0,
+          "target": "0-kitchenmeals"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3 Glenn Court Clayton South",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
